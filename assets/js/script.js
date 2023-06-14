cb9bc5986439f655cbce468f46044592e502f440
Ashley Tan <ashleytan39@gmail.com>
2023-06-13 21:47:44 -0500

    added containers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,49 @@
-var apiKey = 'c090d3c0';
+var apiKey = "c090d3c0";
 function searchMoviesByTitles() {
-  var keywordInput = document.getElementById('searchInput').value.toLowerCase();
-  
+  var keywordInput = document.getElementById("searchInput").value.toLowerCase();
 
-   var apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&type=movie&s=${keywordInput}`;
-   //created Api URL for the keyword.
+  var apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&type=movie&s=${keywordInput}`;
+  //created Api URL for the keyword.
 
   fetch(apiUrl)
-    .then(function(response) {
+    .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
-      if (data.Response === 'True') {
+    .then(function (data) {
+      if (data.Response === "True") {
         var movies = data.Search;
 
-        var movieResult = document.getElementById('movieResult');
-        movieResult.innerHTML = ''; 
+        var movieResult = document.getElementById("movieResult");
+        movieResult.innerHTML = "";
         //clears last result
 
-        movies.forEach(function(movie) {
+        movies.forEach(function (movie) {
           var movieInformationUrl = `https://www.omdbapi.com/?apikey=${apiKey}&type=movie&i=${movie.imdbID}`;
           //created another for loop, for the "movie.imdbID"
 
           fetch(movieInformationUrl)
-            .then(function(response) {
+            .then(function (response) {
               return response.json();
             })
-            .then(function(movieData) {
-              var movieDetails = "Movie Title: " + movieData.Title + ", Year: " + movieData.Year + ", Genre: " + movieData.Genre;
+            .then(function (movieData) {
+              var movieDetails =
+                "Movie Title: " +
+                movieData.Title +
+                ", Year: " +
+                movieData.Year +
+                ", Genre: " +
+                movieData.Genre;
               movieResult.innerHTML += movieDetails + "<br>";
             });
         });
       } else {
-        var movieResult = document.getElementById('movieResult');
-        movieResult.textContent = 'No movies found for the keyword: ' + keywordInput;
+        var movieResult = document.getElementById("movieResult");
+        movieResult.textContent =
+          "No movies found for the keyword: " + keywordInput;
       }
     });
 }
 
-document.getElementById('searchButton').addEventListener('click', searchMoviesByTitles);
+document
+  .getElementById("searchButton")
+  .addEventListener("click", searchMoviesByTitles);
